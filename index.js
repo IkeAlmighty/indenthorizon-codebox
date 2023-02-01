a59f8a6aed18f73b409c0061431d9ddb5d5e8ba2
IkeAlmighty <IsaacYates7@gmail.com>
2023-02-01 11:50:17 -0600

    basic untested implementation

---
+++ index.js
@@ -0,0 +1,25 @@
+const { NodeVM } = require("vm2");
+const WebSocket = require("ws");
+
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+app.post("/run", function (req, res) {
+  const { code, apikey } = JSON.parse(req.body);
+
+  // TODO: check api key
+
+  // Instatiate the VM with access to a websocket:
+  const websocket = new WebSocket();
+  const vm = new NodeVM({ sandbox: { websocket } }); //TODO check to see if I am understanding the purpose of sandbox correctly
+
+  // start the vm:
+  vm.run(code);
+});
+
+app.get("/", function (req, res) {
+  res.redirect("https://github.com/ikealmighty/indenthorizon-codebox");
+});
+
+app.listen(3000);
